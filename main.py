c8db3a125a4d85c4fd62f4dc31550e9e449acb5f
Kayla Kantola <krkantola@gmail.com>
2021-11-26 10:34:35 -0500

    add requests to requirements

--- main.py
+++ main.py
@@ -5,20 +5,41 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-shoutcloud_url = "HTTP://API.SHOUTCLOUD.IO/V1/SHOUT"
-shoutcloud_headers = { 'Content-Type': 'application/json' }
+def read_input(r):
+    request_data_json = json.loads(r.data.decode('utf-8'))
+    return request_data_json['data']
+
+
+def build_shoutcloud_payload(input):
+    shoutcloud_payload = {"INPUT": input}
+    return shoutcloud_payload
+
+
+def post_to_shoutcloud(json_data):
+    shoutcloud_url = "HTTP://API.SHOUTCLOUD.IO/V1/SHOUT"
+    shoutcloud_headers = { 'Content-Type': 'application/json' }
+    shoutcloud_response = requests.request("POST", url=shoutcloud_url, headers=shoutcloud_headers, json=json_data)
+    shotcloud_response_json = shoutcloud_response.json()
+    shoutcloud_output = shotcloud_response_json['OUTPUT']
+    return shoutcloud_output
+
+
+def reverse_text(text):
+    return text[::-1]
+
+
+def build_endpoint_response(text):
+    return {"data": text}
 
 
 @app.route("/", methods=["POST"])
-def hello_world():
+def reverse_shout():
     try:
-        request_data_json = json.loads(request.data.decode('utf-8'))
-        shoutcloud_payload = {"INPUT": request_data_json['data']}
-        shoutcloud_response = requests.request("POST", url=shoutcloud_url, headers=shoutcloud_headers, json=shoutcloud_payload)
-        shotcloud_response_json = shoutcloud_response.json()
-        shoutcloud_output = shotcloud_response_json['OUTPUT']
-        shoutcloud_output_reversed = shoutcloud_output[::-1]
-        endpoint_response = {"data": shoutcloud_output_reversed}
+        user_input = read_input(request)
+        shoutcloud_payload = build_shoutcloud_payload(user_input)
+        shoutcloud_response = post_to_shoutcloud(shoutcloud_payload)
+        shoutcloud_response_reversed = reverse_text(shoutcloud_response)
+        endpoint_response = build_endpoint_response(shoutcloud_response_reversed)
         return endpoint_response
     except Exception as err:
         print(err)
